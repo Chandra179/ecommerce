ad97957918804ddb3d47f64890b51dd7f7ecff38
Chandra179 <chandrafirst67@gmail.com>
2024-08-31 14:18:44 +0700

    add state handler for CSRF

--- internal/server.go
+++ internal/server.go
@@ -15,5 +15,8 @@ func StartServer() {
 	}
 	login := userservice.NewLogin(config)
 	login.HandleLogin()
-	http.ListenAndServe(":8080", nil)
+	err = http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("err")
+	}
 }
